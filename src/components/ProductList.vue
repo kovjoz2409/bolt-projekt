cf76c30f7bf9c7899cc98e639b833f1b89a9f106
Kovács József Miklós <bsze3kovjoz@vasvari.org>
2024-12-18 21:25:47 +0100

    make product list responsive

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -10,12 +10,12 @@ const emit = defineEmits(['add-to-cart']);
 </script>
 
 <template>
-  <BCard
-    v-for="product in products"
-    :key="product.id"
-    :title="`${product.name} ${product.amount.value} ${product.amount.unit}`"
-  >
-    <BCardText>{{ product.price }} Ft</BCardText>
-    <BButton v-if="product.stock > 0" @click="emit('add-to-cart', product)">Kosárba</BButton>
-  </BCard>
+  <BRow cols="1" cols-sm="2" cols-lg="4">
+    <BCol v-for="product in products" :key="product.id" class="text-center p-2">
+      <BCard :title="`${product.name} ${product.amount.value} ${product.amount.unit}`">
+        <BCardText>{{ product.price }} Ft</BCardText>
+        <BButton v-if="product.stock > 0" @click="emit('add-to-cart', product)">Kosárba</BButton>
+      </BCard>
+    </BCol>
+  </BRow>
 </template>
